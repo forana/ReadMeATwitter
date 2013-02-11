3d6751012a0de973ae33ea44fa85d12ebc4202a8
forana <alex@alexforan.com>
2013-02-11 00:55:32 -0600

    Adding blank config

---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+The lines below are meant to be edited. Please edit them. They're for editing.
+"""
+
+# gotten from the twitter app page
+TWITTER_CONSUMER_KEY = ""
+TWITTER_CONSUMER_SECRET = ""
+TWITTER_ACCESS_TOKEN = ""
+TWITTER_ACCESS_SECRET = ""
+
+# command used for speech - uncomment the one you need
+#SPEAK_CMD = "say %s" # osx
+SPEAK_CMD = "espeak %s" # linux - will need to install it first
+
+# seconds between poll attempts
+POLL_INTERVAL = 60
+
+# announce power-on?
+ANNOUNCE_ENABLED = True
+ANNOUNCE_MESSAGE = "Twitter reader ready"
