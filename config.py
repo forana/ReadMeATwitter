44e7ec1883751f88bd745e74e7a1cd26c933deaf
forana <alex@alexforan.com>
2013-02-12 18:16:42 -0600

    removed commands from config
    updated speaker to use google TTS
    
    stupid message removed

--- config.py
+++ config.py
@@ -1,16 +1,8 @@
-"""
-The lines below are meant to be edited. Please edit them. They're for editing.
-"""
-
 # gotten from the twitter app page
 TWITTER_CONSUMER_KEY = ""
 TWITTER_CONSUMER_SECRET = ""
 TWITTER_ACCESS_TOKEN = ""
 TWITTER_ACCESS_SECRET = ""
-
-# command used for speech - uncomment the one you need
-#SPEAK_CMD = ["say"] # osx
-SPEAK_CMD = ["espeak"] # linux - will need to install it first
 
 # minutes between poll attempts
 # twitter rate-limits the call here to 15 times per hour - so at most every 4 mins. set faster at your own risk!
--- speaker.py
+++ speaker.py
@@ -1,6 +1,11 @@
 import subprocess
-import config
 import pipes
+import urllib
+
+CMD = "mpg123 %s 2> /dev/null"
+URL = "http://translate.google.com/translate_tts?tl=en&q=%s"
 
 def speak(text):
-	subprocess.call(config.SPEAK_CMD + [pipes.quote(text)])
+	fullURL = URL % (urllib.quote(text),)
+	fullCMD = CMD % (pipes.quote(fullURL),)
+	subprocess.call(fullCMD, shell = True)
